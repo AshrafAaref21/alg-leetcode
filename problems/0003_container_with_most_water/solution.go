38ae05abf61949d6713576b1bb0a5d8122fc2415
AshrafAaref21 <ashrafaaref2020@gmail.com>
2026-03-17 08:10:17 +0200

    feat: alg modules setup and microsoft container with most water problem.

--- problems/0003_container_with_most_water/solution.go
+++ problems/0003_container_with_most_water/solution.go
@@ -1,5 +1,22 @@
 package p0003_container_with_most_water
 
-func ContainerWithMostWater() {
-	// TODO: implement solution
+func ContainerWithMostWater(height []int) int {
+	maxArea := 0
+	start, end := 0, len(height)-1
+
+	for start < end {
+		left, right := height[start], height[end]
+		area := (end - start) * min(left, right)
+		if area > maxArea {
+			maxArea = area
+		}
+
+		if left < right {
+			start++
+		} else {
+			end--
+		}
+	}
+
+	return maxArea
 }
