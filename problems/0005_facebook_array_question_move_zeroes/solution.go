87f5226da9d5df88e8476cd0a22ead340b3dc7eb
AshrafAaref21 <ashrafaaref2020@gmail.com>
2026-06-07 19:03:07 +0300

    feat: add solution for 283. Move Zeroes.

--- problems/0005_facebook_array_question_move_zeroes/solution.go
+++ problems/0005_facebook_array_question_move_zeroes/solution.go
@@ -1,5 +1,17 @@
 package p0005_facebook_array_question_move_zeroes
 
-func FacebookArrayQuestionMoveZeroes() {
-	// TODO: implement solution
+func FacebookArrayQuestionMoveZeroes(nums []int) {
+	zeroIndex := 0
+	n := len(nums)
+
+	for nonZeroIndex := 0; nonZeroIndex < n; nonZeroIndex++ {
+		if nums[nonZeroIndex] != 0 {
+			nums[zeroIndex] = nums[nonZeroIndex]
+			zeroIndex++
+		}
+	}
+
+	for i := zeroIndex; i < n; i++ {
+		nums[i] = 0
+	}
 }
