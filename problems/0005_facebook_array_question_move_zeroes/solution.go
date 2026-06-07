1fed1b62e940f755c8ec0e2db9bac1d16bfe9af5
AshrafAaref21 <ashrafaaref2020@gmail.com>
2026-06-07 17:57:03 +0300

    feat: add structure 283. Move Zeroes.

---
+++ problems/0005_facebook_array_question_move_zeroes/solution.go
@@ -0,0 +1,5 @@
+package p0005_facebook_array_question_move_zeroes
+
+func FacebookArrayQuestionMoveZeroes() {
+	// TODO: implement solution
+}
---
+++ problems/0005_facebook_array_question_move_zeroes/solution_test.go
@@ -0,0 +1,7 @@
+package p0005_facebook_array_question_move_zeroes
+
+import "testing"
+
+func TestFacebookArrayQuestionMoveZeroes(t *testing.T) {
+	t.Skip("add test cases")
+}
