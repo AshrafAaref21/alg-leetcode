38dc94ed2188940a632c76659a89d37be137cf4d
AshrafAaref21 <ashrafaaref2020@gmail.com>
2026-06-07 19:06:39 +0300

    feat: add time, space complexity for solution for 283. Move Zeroes.

--- problems/0005_facebook_array_question_move_zeroes/solution.go
+++ problems/0005_facebook_array_question_move_zeroes/solution.go
@@ -1,5 +1,9 @@
 package p0005_facebook_array_question_move_zeroes
 
+// https://leetcode.com/problems/move-zeroes/description/
+// time complexity: O(n), where n is the length of the input array
+// space complexity: O(1)
+
 func FacebookArrayQuestionMoveZeroes(nums []int) {
 	zeroIndex := 0
 	n := len(nums)
