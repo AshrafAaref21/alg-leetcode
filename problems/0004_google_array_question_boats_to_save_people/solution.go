45fd1e2fadd6f1cc1092cb2da07980ff019a122b
AshrafAaref21 <ashrafaaref2020@gmail.com>
2026-06-07 16:14:41 +0300

    feat: add solution for 881. Boats to Save People.

--- problems/0004_google_array_question_boats_to_save_people/solution.go
+++ problems/0004_google_array_question_boats_to_save_people/solution.go
@@ -1,8 +1,22 @@
 package p0004_google_array_question_boats_to_save_people
 
+import "sort"
+
 // https://leetcode.com/problems/boats-to-save-people/
 func GoogleArrayQuestionBoatsToSavePeople(people []int, limit int) int {
 	boats := 0
+	sort.Ints(people)
+
+	heavyP := len(people) - 1
+	lightP := 0
+
+	for heavyP >= lightP {
+		if people[heavyP]+people[lightP] <= limit {
+			lightP++
+		}
+		heavyP--
+		boats++
+	}
 
 	return boats
 }
