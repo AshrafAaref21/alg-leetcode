c31cfa95869abcc86997b51175bb475ce4766cb1
AshrafAaref21 <ashrafaaref2020@gmail.com>
2026-06-07 16:14:13 +0300

    feat: add structure 881. Boats to Save People.

---
+++ problems/0004_google_array_question_boats_to_save_people/solution.go
@@ -0,0 +1,8 @@
+package p0004_google_array_question_boats_to_save_people
+
+// https://leetcode.com/problems/boats-to-save-people/
+func GoogleArrayQuestionBoatsToSavePeople(people []int, limit int) int {
+	boats := 0
+
+	return boats
+}
---
+++ problems/0004_google_array_question_boats_to_save_people/solution_test.go
@@ -0,0 +1,7 @@
+package p0004_google_array_question_boats_to_save_people
+
+import "testing"
+
+func TestGoogleArrayQuestionBoatsToSavePeople(t *testing.T) {
+	t.Skip("add test cases")
+}
