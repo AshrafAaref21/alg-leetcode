5c23169784f97c778adc150cae6748c78f559178
AshrafAaref21 <ashrafaaref2020@gmail.com>
2026-06-07 17:51:18 +0300

    feat: add time, space complexity for solution for 881. Boats to Save People.

--- problems/0004_google_array_question_boats_to_save_people/solution.go
+++ problems/0004_google_array_question_boats_to_save_people/solution.go
@@ -3,6 +3,8 @@ package p0004_google_array_question_boats_to_save_people
 import "sort"
 
 // https://leetcode.com/problems/boats-to-save-people/
+// time complexity: O(n log n) due to sorting, where n is the number of people
+// space complexity: O(1) if we ignore the space used by sorting, otherwise O(n) due to sorting
 func GoogleArrayQuestionBoatsToSavePeople(people []int, limit int) int {
 	boats := 0
 	sort.Ints(people)
