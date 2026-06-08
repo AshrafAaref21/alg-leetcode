b8e93e5bb41f7dd9bb6fe0bec05f6e528ea0fa17
AshrafAaref21 <ashrafaaref2020@gmail.com>
2026-06-08 16:11:09 +0300

    feat: add solution for problem 34. Find First and Last Position of Element in Sorted Array

--- problems/0007_arrays_question_find_first_and_last_position_of_element_in_sorted_array/solution.go
+++ problems/0007_arrays_question_find_first_and_last_position_of_element_in_sorted_array/solution.go
@@ -1,5 +1,8 @@
 package p0007_arrays_question_find_first_and_last_position_of_element_in_sorted_array
 
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+// Time Complexity: O(log(n))
+// Space Complexity: O(1)
 func ArraysQuestionFindFirstAndLastPositionOfElementInSortedArray(nums []int, target int) []int {
 	first := findFirst(nums, target)
 	last := findLast(nums, target)
@@ -7,9 +10,37 @@ func ArraysQuestionFindFirstAndLastPositionOfElementInSortedArray(nums []int, ta
 }
 
 func findFirst(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for right >= left {
+		mid := (right + left) / 2
+		if nums[mid] == target {
+			if mid == 0 || nums[mid-1] != target {
+				return mid
+			}
+			right = mid - 1
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
 	return -1
 }
 
 func findLast(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for right >= left {
+		mid := (right + left) / 2
+		if nums[mid] == target {
+			if mid == len(nums)-1 || nums[mid+1] != target {
+				return mid
+			}
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
 	return -1
 }
