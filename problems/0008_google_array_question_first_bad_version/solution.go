f0991d882c4cc7a827a44e0b5d31b610fd647252
AshrafAaref21 <ashrafaaref2020@gmail.com>
2026-06-09 20:35:15 +0300

    feat: add solution for problem 278. First Bad Version

--- problems/0008_google_array_question_first_bad_version/solution.go
+++ problems/0008_google_array_question_first_bad_version/solution.go
@@ -1,5 +1,24 @@
 package p0008_google_array_question_first_bad_version
 
-func GoogleArrayQuestionFirstBadVersion() {
-	// TODO: implement solution
+func GoogleArrayQuestionFirstBadVersion(n int) int {
+	left, right := 1, n
+
+	for left < right {
+		mid := left + (right-left)/2
+
+		if isBadVersion(mid) {
+			if mid == 1 || (mid > 1 && !isBadVersion(mid-1)) {
+				return mid
+			}
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return left
+}
+
+func isBadVersion(version int) bool {
+	return version >= 4
 }
