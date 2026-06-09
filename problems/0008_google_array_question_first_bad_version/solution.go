77b417447061f2b432beb3719dbeeba5af61952d
AshrafAaref21 <ashrafaaref2020@gmail.com>
2026-06-09 20:12:40 +0300

    feat: add structure for problem 278. First Bad Version

---
+++ problems/0008_google_array_question_first_bad_version/solution.go
@@ -0,0 +1,5 @@
+package p0008_google_array_question_first_bad_version
+
+func GoogleArrayQuestionFirstBadVersion() {
+	// TODO: implement solution
+}
---
+++ problems/0008_google_array_question_first_bad_version/solution_test.go
@@ -0,0 +1,7 @@
+package p0008_google_array_question_first_bad_version
+
+import "testing"
+
+func TestGoogleArrayQuestionFirstBadVersion(t *testing.T) {
+	t.Skip("add test cases")
+}
